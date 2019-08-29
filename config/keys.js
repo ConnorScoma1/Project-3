79df06f47015f79591d444a246ede735749e274b
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-08-29 13:14:05 -0500

    removing personal creditals

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,14 @@
+const username = require('../userPass/username')
+
+/* 
+================= USERNAME FILE ===============
 module.exports = {
-    mongoURI: 'mongodb+srv://connorscoma:54377Sco!@cluster0-th3ub.mongodb.net/test?retryWrites=true&w=majority'
+    username: 'YOUR USERNAME',
+    password: 'YOUR PASSWORD'
+}
+*/ 
+
+
+module.exports = {
+    mongoURI: 'mongodb+srv://' + username.username + ': ' + username.password + ' @cluster0-th3ub.mongodb.net/test?retryWrites=true&w=majority'
 }
