5074d918bf810905aba38d8ae74b996435cb525f
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-08-29 12:16:18 -0500

    connecting mongoDB

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+const items = require('./routes/api/items')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected '))
+    .catch(err => console.log(err))
