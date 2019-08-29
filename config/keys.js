11078d5185bb64ff985ba5a65fa08695aa35e5d5
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-08-29 13:08:05 -0500

    adding password

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://connorscoma:<password>@cluster0-th3ub.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://connorscoma:54377Sco!@cluster0-th3ub.mongodb.net/test?retryWrites=true&w=majority'
 }
