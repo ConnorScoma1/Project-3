7f210e0d02308f7172b67218e0bdae9dafbe6a6e
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-08-29 12:31:15 -0500

    crud adding in api route

--- routes/api/items.js
+++ routes/api/items.js
@@ -15,3 +15,11 @@ router.post('/', (req, res) => {
     })
     newItem.save().then(item => res.json(item))
 })
+
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }))
+});
+
+module.exports = router
