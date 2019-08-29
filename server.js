7d5f2f6026cad02bd12a6790eb7b2c0e6b677f01
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-08-29 12:21:07 -0500

    creating server file

--- server.js
+++ server.js
@@ -15,3 +15,17 @@ mongoose
     .connect(db)
     .then(() => console.log('MongoDB Connected '))
     .catch(err => console.log(err))
+
+app.use('/api/items', item);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server Listening on port: ${port}`))
