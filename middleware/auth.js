2b80b353757ee8f5e688786ad664114fa121d46a
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-09-03 08:31:37 -0500

    debugging middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,42 +2,18 @@ const config = require('config');
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
-    const token = req.header('x-auth-token');
+  const token = req.header('x-auth-token');
 
-    if (!token) {
-        res.status(401).json({ msg: "No Token, Denied" })
-    }
+  if (!token)
+    return res.status(401).json({ msg: 'No token, authorizaton denied' });
 
-    try {
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
-        req.user = decoded;
-        next()
-    }
-    catch (e) {
-        res.status(400).json({ msg: 'token not valid' })
-    }
-
-}
-
-module.exports = auth;const config = require('config');
-const jwt = require('jsonwebtoken');
-
-function auth(req, res, next) {
-    const token = req.header('x-auth-token');
-
-    if (!token) {
-        res.status(401).json({ msg: "No Token, Denied" })
-    }
-
-    try {
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
-        req.user = decoded;
-        next()
-    }
-    catch (e) {
-        res.status(400).json({ msg: 'token not valid' })
-    }
-
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: 'Token is not valid' });
+  }
 }
 
 module.exports = auth;
