29384a457b40d5e065535419aa1cc808c7d18942
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-09-03 08:30:43 -0500

    adding middleware block

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,2 +1,43 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+
+    if (!token) {
+        res.status(401).json({ msg: "No Token, Denied" })
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded;
+        next()
+    }
+    catch (e) {
+        res.status(400).json({ msg: 'token not valid' })
+    }
+
+}
+
+module.exports = auth;const config = require('config');
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+
+    if (!token) {
+        res.status(401).json({ msg: "No Token, Denied" })
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded;
+        next()
+    }
+    catch (e) {
+        res.status(400).json({ msg: 'token not valid' })
+    }
+
+}
+
+module.exports = auth;
