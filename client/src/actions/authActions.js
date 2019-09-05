27fec9dbc0196c5555d9acbab118b458970948fa
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-09-05 15:50:41 -0500

    axios get token

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -11,3 +11,22 @@ import {
 } from './types';
 
 // Check Token
+export const loadUser = () => (dispatch, getState) => {
+    dispatch({ type: USER_LOADING });
+
+    // Get Token
+    const token = getState().auth.token;
+
+    // Header
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    if(token) {
+        config.headers['x-auth-token'] = token;
+    }
+
+    axios.get('/api/auth/user',)
+}   
