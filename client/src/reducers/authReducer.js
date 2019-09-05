8c9bf9db7b0dc85bc2c388bb2f4149276faff508
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-09-05 15:19:41 -0500

    localstorage token search

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,28 @@
+import {
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from '../actions/types';
+import { stat } from 'fs';
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    isLoading: false,
+    user: null
+};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case USER_LOADING:  
+            return {
+                ...state,
+                isLoading: true
+            }
+    }
+}
