b0ef55b8a85f4b335e1aed6694c2aad786facbb0
Connor Scoma <connorscoma@live.com>
2019-09-07 12:08:24 -0500

    adding new scripts

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import React, { Component }  from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/index";
 
-import store from './store';
-import { loadUser } from './actions/authActions';
+// import store from './store';
+// import { loadUser } from './actions/authActions';
 
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    store.dispatch(loadUser());
-  }
+  // componentDidMount() {
+  //   store.dispatch(loadUser());
+  // }
   render() {
     return (
       <Router>
