f5a55c1a7ceb8b981f565c7fd38da2804d172df2
root <chinenyediei@gmail.com>
2019-09-10 21:44:47 -0500

    inventory model added

---
+++ models/Inventory.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const InventorySchema = new Schema({
+
+    
+    productName: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: INTEGER,
+        default: Date.now
+    }
+})
+
+module.exports = Inventory = mongoose.model('user', InventorySchema);
