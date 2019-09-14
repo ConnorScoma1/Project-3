192ccdbae899c44328148425f4b4353f28cbe192
Connor Scoma <connorscoma@live.com>
2019-09-14 10:16:52 -0500

    adding new package and nav function

--- client/src/components/Login/component/protectComponents/Nav/Nav.js
+++ client/src/components/Login/component/protectComponents/Nav/Nav.js
@@ -1,23 +1,43 @@
 import React, { Component } from "react";
-import './App.css'
+import "./App.css";
+
+import { Tabs, Tab, Grid, Cell } from "react-mdl";
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
 
-    render() {
-        return (
-            <div className="nav-background">
-                <ul className="nav-ul">
-                    <li><a href="/protected">Dashboard</a></li>
-                    <li><a href="/protected/Inventory">Inventory</a></li>
-                    <li><a href="/protected/Manage">Manage</a></li>
-                    <li><a href="/protected/Account">Account</a></li>
-                    <li><a href="/protected/Support">Support</a></li>
-                    <li><a href="/protected/Contact">Contact</a></li>
-                </ul>
-            </div>
-        )
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return <div>This a Dashboard Component</div>;
+    } else if (this.state.activeTab === 1) {
+      return <div>This is Inventory</div>;
+    } else if (this.state.activeTab === 2) {
+      return <div>This is Manage</div>;
+    } else if (this.state.activeTab === 3) {
+      return <div>This is Account</div>;
+    } else if (this.state.activeTab === 4) {
+      return <div>This is Support</div>;
+    } else if (this.state.activeTab === 5) {
+      return <div>This is Contact</div>;
     }
+  }
+  render() {
+    return (
+      <div className="nav-background">
+        <ul className="nav-ul">
+          <Tab className="nav-item"> Dashboard </Tab>
+          <Tab className="nav-item"> Inventory </Tab>
+          <Tab className="nav-item"> Manage </Tab>
+          <Tab className="nav-item"> Account </Tab>
+          <Tab className="nav-item"> Support </Tab>
+          <Tab className="nav-item"> Contact </Tab>
+        </ul>
+      </div>
+    );
+  }
 }
-
 
 export default Nav;
